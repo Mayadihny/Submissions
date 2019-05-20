2d93705fef2f2d9468d732f7b6265b143bd3a27f
Mayadihny <mayadihny@gmail.com>
2019-05-20 14:39:52 +0300

    Basic1 step4

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,4 @@
+var a=prompt("enter your name");
+var b=prompt("enter your suname");
+var c=prompt("enter your city");
+alert("name :" + " " + a + "\n" + "surname :" + " " + b + "\n" +"City :" + " " + c);
