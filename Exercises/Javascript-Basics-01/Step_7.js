d6b004801a7f0e3fd31a29b66ed818f7e693657b
Mayadihny <mayadihny@gmail.com>
2019-05-20 14:54:34 +0300

    Basic1 step7

---
+++ Exercises/Javascript-Basics-01/Step_7.js
@@ -0,0 +1,14 @@
+var a=prompt("enter your shoe size");
+var b=prompt("enter your Birth year");
+
+function funone(a, b) {
+    var c=(a*2);
+    var d=(c+5);
+    var e=(d*50);
+    var f=(e-b);
+    var g=(f+1766);
+    return g;
+}
+
+alert(funone(a, b));
+
