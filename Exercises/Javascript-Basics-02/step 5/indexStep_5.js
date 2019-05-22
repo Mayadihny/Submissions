68323a83bb21740b1a358d576eac88c93548b482
Mayadihny <mayadihny@gmail.com>
2019-05-22 20:38:24 +0300

    step 6

--- Exercises/Javascript-Basics-02/step 5/indexStep_5.js
+++ Exercises/Javascript-Basics-02/step 5/indexStep_5.js
@@ -1,81 +1,40 @@
-// var img1 = document.querySelector("#image1");
-// var img2 = document.querySelector("#image2");
-// var img3 = document.querySelector("#image3");
-// var img4 = document.querySelector("#image4");
-// var img5 = document.querySelector("#image5");
-
-// img1.addEventListener("mouseover", function() {
-//   img1.src="images/image1_2.jpg"; 
-// })
-// img1.addEventListener("mouseout", function() {
-//   img1.src="images/image1.jpg";
-// })
-
-// img2.addEventListener("mouseover", function() {
-//     img2.src="images/image2_2.jpg"; 
-//   })
-//   img2.addEventListener("mouseout", function() {
-//     img2.src="images/image2.jpg";
-//   })
-
-//   img3.addEventListener("mouseover", function() {
-//     img3.src="images/image3_2.jpg"; 
-//   })
-//   img3.addEventListener("mouseout", function() {
-//     img3.src="images/image3.jpg";
-//   })
-
-//   img4.addEventListener("mouseover", function() {
-//     img4.src="images/image4_2.jpg"; 
-//   })
-//   img4.addEventListener("mouseout", function() {
-//     img4.src="images/image4.jpg";
-//   })
-
-//   img5.addEventListener("mouseover", function() {
-//     img5.src="images/image5_2.jpg"; 
-//   })
-//   img5.addEventListener("mouseout", function() {
-//     img5.src="images/image5.jpg";
-//   })
-
 var img1 = document.querySelector("#image1");
 var img2 = document.querySelector("#image2");
 var img3 = document.querySelector("#image3");
 var img4 = document.querySelector("#image4");
 var img5 = document.querySelector("#image5");
 
-img1.addEventListener("mouseover", imgs)
-img2.addEventListener("mouseover", imgs)
-img3.addEventListener("mouseover", imgs)
-img4.addEventListener("mouseover", imgs)
-img5.addEventListener("mouseover", imgs)
+img1.addEventListener("mouseover", function() {
+  img1.src="images/image1_2.jpg"; 
+})
+img1.addEventListener("mouseout", function() {
+  img1.src="images/image1.jpg";
+})
 
-img1.addEventListener("mouseout", imgs)
-img2.addEventListener("mouseout", imgs)
-img3.addEventListener("mouseout", imgs)
-img4.addEventListener("mouseout", imgs)
-img5.addEventListener("mouseout", imgs)
+img2.addEventListener("mouseover", function() {
+    img2.src="images/image2_2.jpg"; 
+  })
+  img2.addEventListener("mouseout", function() {
+    img2.src="images/image2.jpg";
+  })
 
-function imgs(ev){
-    if (ev.target.id == "image1")
-    {    ev.target.src = "images/image1_2.jpg" 
+  img3.addEventListener("mouseover", function() {
+    img3.src="images/image3_2.jpg"; 
+  })
+  img3.addEventListener("mouseout", function() {
+    img3.src="images/image3.jpg";
+  })
 
-}
-else if(ev.target.id == "image2"){
-ev.target.src = "images/image2_2.jpg"
+  img4.addEventListener("mouseover", function() {
+    img4.src="images/image4_2.jpg"; 
+  })
+  img4.addEventListener("mouseout", function() {
+    img4.src="images/image4.jpg";
+  })
 
-}
-else if (ev.target.id == "image3"){
-    ev.target.src ="images/image3_2.jpg"
-}
-else if (ev.target.id == "image4"){
-ev.target.src = "images/image4_2.jpg"
-}
-
-else if (ev.target.id== "image5")
-{
-    ev.target.src = "images/image5_2.jpg"
-}
-}
- 
+  img5.addEventListener("mouseover", function() {
+    img5.src="images/image5_2.jpg"; 
+  })
+  img5.addEventListener("mouseout", function() {
+    img5.src="images/image5.jpg";
+  })
