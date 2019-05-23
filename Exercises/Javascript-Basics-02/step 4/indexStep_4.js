363403426710a666191d38283ffddfe110304295
Mayadihny <mayadihny@gmail.com>
2019-05-23 12:57:12 +0300

    step 4

--- Exercises/Javascript-Basics-02/step 4/indexStep_4.js
+++ Exercises/Javascript-Basics-02/step 4/indexStep_4.js
@@ -1,8 +1,8 @@
 var a = document.querySelector("button");
 var b = document.querySelector("input");
-a.addEventListener("click", lol)
-function lol () { 
-   var lmao ;
+a.addEventListener("click", myFunction)
+function myFunction() { 
+   var c ;
    if (confirm("yes or no")) {
        yes=document.getElementById("name").value=" ";
        document.getElementById("surname").value=" ";
