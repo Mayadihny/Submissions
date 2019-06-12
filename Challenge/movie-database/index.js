0ef617870886488bff7314c7de9ddd4a4db07a94
Mayadihny <mayadihny@gmail.com>
2019-06-12 10:46:52 +0300

    Step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
 res.send('<h1>Ok</h1>');
 });
 
+app.get('/test', (req, res) => {
+    res.send({status:200, message:"ok"});
+    });
+    
+app.get('/time', (req, res) => {
+        var time = new Date ();
+        res.send({status:200, message: time.getHours() + ":"+ time.getMinutes() + ":" + time.getSeconds()});
+        });
+        
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
