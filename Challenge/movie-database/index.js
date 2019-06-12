d8bc21850e1415470bfd43a21a9a56a74a505cef
Mayadihny <mayadihny@gmail.com>
2019-06-12 18:21:33 +0300

    Step5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app= express();
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 app.get('/', (req, res) => {
 res.send('<h1>Ok</h1>');
 });
@@ -26,6 +33,22 @@ app.get('/search', (req, res) =>{
         res.send({status:200, message:"ok", data:req.query.s})}
     else { res.send({status:500, error:true, message:"you have to provide a search"})} 
         })
+
+app.get('/movies/create', (req, res) => {
+    res.send('<h1>Ok</h1>');
+});
+
+app.get('/movies/read', (req, res) => {
+    res.send({status:200, data: movies})
+});
+
+app.get('/movies/update', (req, res) => {
+    res.send('<h1>Ok</h1>');
+});
+
+app.get('/movies/delete', (req, res) => {
+    res.send('<h1>Ok</h1>');
+});
 
 const PORT = process.env.PORT || 5000;
 
